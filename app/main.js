f9d300163a1548b2db58959203d8264a21c9db4e
leepavelich <leepavelich@gmail.com>
2024-03-13 10:38:57 -0600

    Stage 3

--- app/main.js
+++ app/main.js
@@ -2,8 +2,11 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    console.log(data.toString());
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    if (data.toString().includes("GET / HTTP/1.1")) {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n");
+    }
   });
 
   socket.on("close", () => {
