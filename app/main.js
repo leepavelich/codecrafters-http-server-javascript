ee1f98d58ffe9f0b16d87fca2d06d433917b9c58
leepavelich <leepavelich@gmail.com>
2024-03-13 11:00:48 -0600

    Stage 4

--- app/main.js
+++ app/main.js
@@ -3,25 +3,18 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const d = data.toString();
-    if (d.startsWith("GET")) {
-      const [get, path, httpVersion] = d.split(" ");
-      if (path === "/") {
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      } else if (path.startsWith("/echo/")) {
-        const str = path.split("/").at(-1);
-        // HTTP/1.1 200 OK
-        // Content-Type: text/plain
-        // Content-Length: 3
 
-        // abc
-        socket.write(`
-        HTTP/1.1 200 OK\r\n
-        Content-Type: text/plain\r\n
-        Content-Length: ${str.length}\r\n
-        \r\n
-        ${str}\r\n`);
+    if (d.startsWith("GET")) {
+      const [_get, path, _httpVersion] = d.split(" ");
+
+      if (path === "/") {
+        socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
+      } else if (path.startsWith("/echo/")) {
+        const str = path.slice(6);
+        const res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}\r\n`;
+        socket.write(res);
       } else {
-        socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n");
+        socket.write(`HTTP/1.1 404 NOT FOUND\r\n\r\n`);
       }
     }
   });
