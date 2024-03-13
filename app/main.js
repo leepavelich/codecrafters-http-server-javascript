672c4d840bac237fe9501d213fadfe4ca615d9f5
leepavelich <leepavelich@gmail.com>
2024-03-13 10:45:01 -0600

    scope fix

--- app/main.js
+++ app/main.js
@@ -5,14 +5,14 @@ const server = net.createServer((socket) => {
     const d = data.toString();
     if (d.startsWith("GET")) {
       const [get, path, httpVersion] = d.split(" ");
-    }
-    if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (path.startsWith("/echo/")) {
-      const str = path.split("/").at(-1);
-      console.log(str);
-    } else {
-      socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n");
+      if (path === "/") {
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      } else if (path.startsWith("/echo/")) {
+        const str = path.split("/").at(-1);
+        console.log(str);
+      } else {
+        socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n");
+      }
     }
   });
 
