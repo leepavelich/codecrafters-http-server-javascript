bf36e431b0412c461f243c2426b6c3126ec3414d
leepavelich <leepavelich@gmail.com>
2024-03-13 10:44:20 -0600

    Extract string

--- app/main.js
+++ app/main.js
@@ -2,8 +2,15 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    if (data.toString().includes("GET / HTTP/1.1")) {
+    const d = data.toString();
+    if (d.startsWith("GET")) {
+      const [get, path, httpVersion] = d.split(" ");
+    }
+    if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (path.startsWith("/echo/")) {
+      const str = path.split("/").at(-1);
+      console.log(str);
     } else {
       socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n");
     }
