9acabf6366a943afd804ecb2ae815dd379855ad8
leepavelich <leepavelich@gmail.com>
2024-03-13 11:58:37 -0600

    Stage 7

--- app/main.js
+++ app/main.js
@@ -1,4 +1,13 @@
 const net = require("net");
+const fs = require("fs");
+
+let directory;
+
+if (process.argv.length === 4) {
+  if (process.argv[2] == "--directory") {
+    directory = process.argv[3];
+  }
+}
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
@@ -17,9 +26,18 @@ const server = net.createServer((socket) => {
       } else if (path === "/user-agent") {
         const userAgent = headers.find((h) => h.startsWith("User-Agen"));
         const str = userAgent.slice(12);
-        console.log(str);
         const res = `${httpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}\r\n`;
         socket.write(res);
+      } else if (path.startsWith("/files/")) {
+        const fileName = path.slice(7);
+        if (fs.existsSync(`${directory}${fileName}`)) {
+          const content = fs.readFileSync(`${directory}${fileName}`, "utf-8");
+          const res = `${httpVersion} 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
+          socket.write(res);
+        } else {
+          socket.write(`${httpVersion} 404 NOT FOUND\r\n\n`);
+          socket.end();
+        }
       } else {
         socket.write(`${httpVersion} 404 NOT FOUND\r\n\r\n`);
       }
