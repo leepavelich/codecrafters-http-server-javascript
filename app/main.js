a3f440202a81b6e7ea277dea6b53e98fbcbc16c5
leepavelich <leepavelich@gmail.com>
2024-03-13 10:33:30 -0600

    Stage 2

--- app/main.js
+++ app/main.js
@@ -4,11 +4,15 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
- const server = net.createServer((socket) => {
-   socket.on("close", () => {
-     socket.end();
-     server.close();
-   });
- });
+const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  });
 
- server.listen(4221, "localhost");
+  socket.on("close", () => {
+    socket.end();
+    server.close();
+  });
+});
+
+server.listen(4221, "localhost");
